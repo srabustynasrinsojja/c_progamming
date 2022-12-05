ac4b23344a4ff7e0a0cf2e7b19ffa63c99938fd9
srabustynasrinsojja <sojjasrabustynasrin@gmail.com>
2022-12-05 16:52:02 +0600

    Arrey

---
+++ lab3_prblm1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int grade_marks;
+    printf("Enter the number:");
+    scanf("%d",&grade_marks);
+    if(grade_marks>=60)
+    printf("\n Passed\n");
+    return 0;
+}
